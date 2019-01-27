4ade989f34322298e90817846600d56accf75b52
rybradlax <34609432+rybradlax@users.noreply.github.com>
2019-01-27 16:31:15 -0500

    Atbash Ciphering/Deciphering
    This script can cipher or decipher letters in the Atbash Cipher.

---
+++ atbash.py
@@ -0,0 +1,58 @@
+print("Welcome to message ciphering using the atbash cipher, to cipher and decipher messages, input the letters one by one to discover what they mean, or to cipher them")
+while True:
+    input1 = input("Input letter here: ")
+    if input1 == "a":
+        print("z")
+    elif input1 == "b":
+        print("y")
+    elif input1 == "c":
+        print("x")
+    elif input1 == "d":
+        print("w")
+    elif input1 == "e":
+        print("v")
+    elif input1 == "f":
+        print("u")
+    elif input1 == "g":
+        print("t")
+    elif input1 == "h":
+        print("s")
+    elif input1 == "i":
+        print("r")
+    elif input1 == "j":
+        print("q")
+    elif input1 == "k":
+        print("p")
+    elif input1 == "l":
+        print("o")
+    elif input1 == "m":
+        print("n")
+    elif input1 == "n":
+        print("m")
+    elif input1 == "o":
+        print("l")
+    elif input1 == "p":
+        print("k")
+    elif input1 == "q":
+        print("j")
+    elif input1 == "r":
+        print("i")
+    elif input1 == "s":
+        print("h")
+    elif input1 == "t":
+        print("g")
+    elif input1 == "u":
+        print("f")
+    elif input1 == "v":
+        print("e")
+    elif input1 == "w":
+        print("d")
+    elif input1 == "x":
+        print("c")
+    elif input1 == "y":
+        print("b")
+    elif input1 == "z":
+        print("a")
+    else:
+        print("Oops! Something was typed in incorrectly, try again, just this time, make the letter lowercase, and input one letter at a time")
+    
