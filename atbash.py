f9899bec1173b869ff4c3d30d56bf957c32f58dc
rybradlax <34609432+rybradlax@users.noreply.github.com>
2019-03-17 13:55:19 -0400

    Update atbash.py

--- atbash.py
+++ atbash.py
@@ -1,58 +1,58 @@
 print("Welcome to message ciphering using the atbash cipher, to cipher and decipher messages, input the letters one by one to discover what they mean, or to cipher them")
 while True:
     input1 = input("Input letter here: ")
-    if input1 == "a":
+    if input1 == "a" or "A":
         print("z")
-    elif input1 == "b":
+    elif input1 == "b" or "B":
         print("y")
-    elif input1 == "c":
+    elif input1 == "c" or "C":
         print("x")
-    elif input1 == "d":
+    elif input1 == "d" or "D":
         print("w")
-    elif input1 == "e":
+    elif input1 == "e" or "D":
         print("v")
-    elif input1 == "f":
+    elif input1 == "f" or "F":
         print("u")
-    elif input1 == "g":
+    elif input1 == "g" or "G":
         print("t")
-    elif input1 == "h":
+    elif input1 == "h" or "H":
         print("s")
-    elif input1 == "i":
+    elif input1 == "i" or "I":
         print("r")
-    elif input1 == "j":
+    elif input1 == "j" "J":
         print("q")
-    elif input1 == "k":
+    elif input1 == "k" or "K":
         print("p")
-    elif input1 == "l":
+    elif input1 == "l" or "L":
         print("o")
-    elif input1 == "m":
+    elif input1 == "m" or "M":
         print("n")
-    elif input1 == "n":
+    elif input1 == "n" or "N":
         print("m")
-    elif input1 == "o":
+    elif input1 == "o" or "O":
         print("l")
-    elif input1 == "p":
+    elif input1 == "p" or "P":
         print("k")
-    elif input1 == "q":
+    elif input1 == "q" or "Q":
         print("j")
-    elif input1 == "r":
+    elif input1 == "r" or "R":
         print("i")
-    elif input1 == "s":
+    elif input1 == "s" or "S":
         print("h")
-    elif input1 == "t":
+    elif input1 == "t" or "T":
         print("g")
-    elif input1 == "u":
+    elif input1 == "u" or "U":
         print("f")
-    elif input1 == "v":
+    elif input1 == "v" or "V":
         print("e")
-    elif input1 == "w":
+    elif input1 == "w" or "W":
         print("d")
-    elif input1 == "x":
+    elif input1 == "x" or "X":
         print("c")
-    elif input1 == "y":
+    elif input1 == "y" or "Y":
         print("b")
-    elif input1 == "z":
+    elif input1 == "z" or "Z":
         print("a")
     else:
-        print("Oops! Something was typed in incorrectly, try again, just this time, make the letter lowercase, and input one letter at a time")
+        print("Oops! Something was typed in incorrectly, try again, input one letter at a time, and make sure something is inputted")
     
